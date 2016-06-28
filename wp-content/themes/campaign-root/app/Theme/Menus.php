aac3926f4fd34364fe66759c7a85104575f6d68b
Alex Sexton <alex@dxw.com>
2016-06-28 17:04:14 +0100

    Refactored clean menu functions so they work

--- wp-content/themes/campaign-root/app/Theme/Menus.php
+++ wp-content/themes/campaign-root/app/Theme/Menus.php
@@ -4,15 +4,9 @@ namespace Dxw\GdsCampaignRoot\Theme;
 
 class Menus implements \Dxw\Iguana\Registerable
 {
-    public function register()
-    {
-        register_nav_menu('header', 'Header Menu');
-        register_nav_menu('footer', 'Footer Menu');
 
-        //Deletes all CSS classes and id's, except for those listed in the array below
-        //Modified from https://gist.github.com/lekkerduidelijk/5576437
-        function custom_wp_nav_menu($var) {
-          return is_array($var) ? array_intersect($var, array(
+    public function custom_wp_nav_menu($var) {
+        return is_array($var) ? array_intersect($var, array(
             //List of allowed menu classes
             'current_page_item',
             'current_page_parent',
@@ -25,16 +19,12 @@ class Menus implements \Dxw\Iguana\Registerable
             'menu-item-has-children',
             'sub-menu'
             )
-          ) : '';
-        }
+        ) : '';
+    }
 
-        add_filter('nav_menu_css_class', 'custom_wp_nav_menu');
-        add_filter('nav_menu_item_id', 'custom_wp_nav_menu');
-        add_filter('page_css_class', 'custom_wp_nav_menu');
-
-        //Replaces "current-menu-item" with "active"
-        function current_to_active($text){
-          $replace = array(
+    //Replaces "current-menu-item" with "active"
+    public function current_to_active($text){
+        $replace = array(
             //List of menu item classes that should be changed to "active"
             'current_page_item' => 'active',
             'current_page_parent' => 'active',
@@ -42,17 +32,23 @@ class Menus implements \Dxw\Iguana\Registerable
             'current-menu-item' => 'active',
             'menu-item-has-children' => 'has-sub-nav',
             'sub-menu' => 'sub-nav',
-          );
-          $text = str_replace(array_keys($replace), $replace, $text);
-            return $text;
-          }
-        add_filter ('wp_nav_menu','current_to_active');
+        );
+        $text = str_replace(array_keys($replace), $replace, $text);
+        return $text;
+    }
 
-        //Deletes empty classes
-        function strip_empty_classes($menu) {
-            return $menu;
-        }
-        add_filter ('wp_nav_menu','strip_empty_classes');
+    public function strip_empty_classes($menu) {
+        return $menu;
+    }
 
+    public function register()
+    {
+        register_nav_menu('header', 'Header Menu');
+        register_nav_menu('footer', 'Footer Menu');
+        add_filter('nav_menu_css_class', [$this, 'custom_wp_nav_menu']);
+        add_filter('nav_menu_item_id', [$this, 'custom_wp_nav_menu']);
+        add_filter('page_css_class', [$this, 'custom_wp_nav_menu']);
+        add_filter ('wp_nav_menu', [$this, 'current_to_active']);
+        add_filter ('wp_nav_menu', [$this, 'strip_empty_classes']);
     }
 }
