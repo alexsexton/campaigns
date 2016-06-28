c3372edbc43bba3039a70d61f7e85758e1f71d64
Alex Sexton <alex@dxw.com>
2016-06-28 17:07:11 +0100

    Main navigation dependencies

--- wp-content/themes/campaign-root/assets/js/main.js
+++ wp-content/themes/campaign-root/assets/js/main.js
@@ -1,3 +1,29 @@
+/* globals jQuery */
+
 'use strict'
 
 require('./common')
+var enquire = require('./plugins/enquire')
+require('./plugins/jquery-accessibleMegaMenu')
+
+jQuery(function ($) {
+  $(function () {
+    enquire.register('screen and (min-width:500px)', {
+      match: function () {
+        // Main Nav
+        $('.menu-main-container').accessibleMegaMenu({
+          uuidPrefix: 'accessible-nav',
+          menuClass: 'nav-menu',
+          topNavItemClass: 'nav-item',
+          panelClass: 'sub-nav',
+          panelGroupClass: 'sub-nav-group',
+          hoverClass: 'hover',
+          focusClass: 'focus',
+          openClass: 'open'
+        })
+      },
+      unmatch: function () {}
+    })
+  })
+// end of block
+})
