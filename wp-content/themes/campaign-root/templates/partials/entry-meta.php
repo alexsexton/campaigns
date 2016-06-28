e093b857e23ae60d54db60a19dbc847a309194e9
Alex Sexton <alex@dxw.com>
2016-06-28 12:01:41 +0100

    Reformat time

--- wp-content/themes/campaign-root/templates/partials/entry-meta.php
+++ wp-content/themes/campaign-root/templates/partials/entry-meta.php
@@ -1,6 +1,6 @@
 <ul class="entry-meta">
     <li>
-        <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+        <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date('l jS F, Y'); ?></time>
     </li>
     <li>
         <span class="author"><?php echo __('By', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></span>
