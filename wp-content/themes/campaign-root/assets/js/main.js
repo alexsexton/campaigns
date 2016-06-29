ba9227993c04bd5e1c7b59d71501c5445e7b1ae2
Alex Sexton <alex@dxw.com>
2016-06-29 15:37:46 +0100

    Add mega menu calls

--- wp-content/themes/campaign-root/assets/js/main.js
+++ wp-content/themes/campaign-root/assets/js/main.js
@@ -7,11 +7,12 @@ var enquire = require('./plugins/enquire')
 require('./plugins/jquery-accessibleMegaMenu')
 
 jQuery(function ($) {
+  // Mega menu
   $(function () {
     enquire.register('screen and (min-width:500px)', {
       match: function () {
         // Main Nav
-        $('.menu-main-container').accessibleMegaMenu({
+        $('.menu-header-container').accessibleMegaMenu({
           uuidPrefix: 'accessible-nav',
           menuClass: 'nav-menu',
           topNavItemClass: 'nav-item',
@@ -25,5 +26,23 @@ jQuery(function ($) {
       unmatch: function () {}
     })
   })
-// end of block
+
+  // Extend jQuery to make a toggle text function.
+  jQuery.fn.extend({
+    toggleText: function (stateOne, stateTwo) {
+      return this.each(function () {
+        stateTwo = stateTwo || ''
+        $(this).text() !== stateTwo && stateOne ? $(this).text(stateTwo) : $(this).text(stateOne)
+      })
+    }
+  })
+
+  // Toggle navigation
+  $(function () {
+    $('#js-navigation-toggle').click(function () {
+      $(this).toggleText('Close', 'Menu')
+      $('#js-navigation').toggleClass('open')
+    })
+  })
+// end
 })
