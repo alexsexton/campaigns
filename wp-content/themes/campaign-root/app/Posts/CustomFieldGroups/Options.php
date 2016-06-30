2673b764c44bb93eafffdc307824c248d02bac2e
Alex Sexton <alex@dxw.com>
2016-06-30 14:44:52 +0100

    Add register function

--- wp-content/themes/campaign-root/app/Posts/CustomFieldGroups/Options.php
+++ wp-content/themes/campaign-root/app/Posts/CustomFieldGroups/Options.php
@@ -1,16 +1,20 @@
 <?php
 
-namespace Dxw\GdsCampaignRoot\Posts\CustomFieldGroups; 
+namespace Dxw\GdsCampaignRoot\Posts\CustomFieldGroups;
 
 class Options implements \Dxw\Iguana\Registerable {
 
-    if (function_exists('acf_add_options_sub_page')) {
-    acf_add_options_sub_page('Homepage');
-    acf_add_options_sub_page('Error page');
-    }
+    public function register() {
 
-    if (function_exists('register_options_page')) {
-    register_options_page('Homepage');
+        if (function_exists('acf_add_options_sub_page')) {
+            acf_add_options_sub_page('Homepage');
+            acf_add_options_sub_page('Error page');
+        }
+
+        if (function_exists('register_options_page')) {
+            register_options_page('Homepage');
+        }
+
     }
 
 }
