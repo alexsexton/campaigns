05a77f1738218a88ef86f14111b12c15ecdb36d9
Alex Sexton <alex@dxw.com>
2016-06-30 17:03:33 +0100

    Removed both paths

--- wp-content/themes/campaign-root/app/Posts/CustomFields.php
+++ wp-content/themes/campaign-root/app/Posts/CustomFields.php
@@ -6,7 +6,6 @@ class CustomFields implements \Dxw\Iguana\Registerable
 {
     public function register()
     {
-        __DIR__ . '/CustomFieldGroups/Options.php';
-        __DIR__ . '/CustomFieldGroups/ContentBlocks.php';
+        // ...
     }
 }
