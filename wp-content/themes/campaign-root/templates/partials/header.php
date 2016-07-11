98d1d429d64d9e2a528de9d65e1387bb9ed3ba22
Alex Sexton <alex@dxw.com>
2016-07-11 17:02:34 +0100

    Initial options header output

--- wp-content/themes/campaign-root/templates/partials/header.php
+++ wp-content/themes/campaign-root/templates/partials/header.php
@@ -1,7 +1,17 @@
+<?php if( get_field('header_background_colour', 'option')) : ?>
+<header role="banner" id="site-header" class="site-header" style="background-color:<?php the_field('header_background_colour', 'option'); ?>;">
+<?php else : ?>
 <header role="banner" id="site-header" class="site-header">
+<?php endif ?>
     <div class="row">
         <div class="logo">
-            <a href="/"><h1><?php bloginfo('name'); ?></h1></a>
+            <a href="/"></a>
+            <?php if( get_field('site_logo', 'option') ) : ?>
+                <?php $site_logo = get_field('site_logo', 'option'); ?>
+                <img src="<?php echo $site_logo['url']; ?>" alt="<?php bloginfo('name'); ?>">
+            <?php else : ?>
+                <h1><?php bloginfo('name'); ?></h1>
+            <?php endif ?>
         </div>
         <?php if (get_bloginfo('description')) : ?>
             <div class="description">
