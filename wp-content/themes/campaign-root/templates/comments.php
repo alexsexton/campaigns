10158223cdad6c21f3a3dfc98d9a94091409e052
Alex Sexton <alex@dxw.com>
2016-07-18 11:51:11 +0100

    Updated comments templates

--- wp-content/themes/campaign-root/templates/comments.php
+++ wp-content/themes/campaign-root/templates/comments.php
@@ -7,7 +7,7 @@ if (post_password_required()) {
 
 <?php if (have_comments()) : ?>
 
-    <section id="comments">
+    <section id="comments" class="comments row">
         <h3><?php printf(_n('One Response to &ldquo;%2$s&rdquo;', '%1$s Responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'roots'), number_format_i18n(get_comments_number()), get_the_title()); ?></h3>
 
         <ol class="media-list">
@@ -15,16 +15,14 @@ if (post_password_required()) {
         </ol>
 
         <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
-            <nav>
-                <ul class="pager">
-                    <?php if (get_previous_comments_link()) : ?>
-                        <li class="previous"><?php previous_comments_link(__('&larr; Older comments', 'roots')); ?></li>
-                    <?php endif; ?>
-                    <?php if (get_next_comments_link()) : ?>
-                        <li class="next"><?php next_comments_link(__('Newer comments &rarr;', 'roots')); ?></li>
-                    <?php endif; ?>
-                </ul>
-            </nav>
+            <ul class="pager">
+                <?php if (get_previous_comments_link()) : ?>
+                    <li class="previous"><?php previous_comments_link(__('&larr; Older comments', 'roots')); ?></li>
+                <?php endif; ?>
+                <?php if (get_next_comments_link()) : ?>
+                    <li class="next"><?php next_comments_link(__('Newer comments &rarr;', 'roots')); ?></li>
+                <?php endif; ?>
+            </ul>
         <?php endif; ?>
     </section>
 <?php endif; ?>
--- wp-content/themes/campaign-root/templates/partials/comment-form.php
+++ wp-content/themes/campaign-root/templates/partials/comment-form.php
@@ -1,4 +1,4 @@
-<section id="comment-form" class="col-md-12">
+<section id="comment-form" class="comment-form row">
     <h3><?php comment_form_title(__('Leave a Reply', 'roots'), __('Leave a Reply to %s', 'roots')); ?></h3>
     <p class="cancel-comment-reply"><?php cancel_comment_reply_link(); ?></p>
     <?php if (get_option('comment_registration') && !is_user_logged_in()) : ?>
@@ -28,7 +28,7 @@
                 <label for="comment"><?php _e('Comment', 'roots'); ?></label>
                 <textarea name="comment" id="comment" class="form-control" rows="5" aria-required="true"></textarea>
             </div>
-            <p><input name="submit" class="btn btn-primary" type="submit" id="submit" value="<?php _e('Submit Comment', 'roots'); ?>"></p>
+            <div class="form-buttons"><input name="submit" class="button" type="submit" id="submit" value="<?php _e('Submit Comment', 'roots'); ?>"></div>
             <?php comment_id_fields(); ?>
             <?php do_action('comment_form', $post->ID); ?>
         </form>
