e66bad2d4623b4d0b78d4931cd8c2aee24356581
Alex Sexton <alex@dxw.com>
2016-07-18 12:07:14 +0100

    Update entry meta and read more link

--- wp-content/themes/campaign-root/templates/partials/article-list-item.php
+++ wp-content/themes/campaign-root/templates/partials/article-list-item.php
@@ -1,4 +1,4 @@
-<article <?php post_class('article-list-item'); ?>
+<article <?php post_class('article-list-item'); ?>>
     <header>
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <?php get_template_part('partials/entry-meta'); ?>
@@ -9,6 +9,10 @@
             <?php the_post_thumbnail('large'); ?>
         <?php endif; ?>
         <?php the_excerpt(); ?>
-        <a href="<?php the_permalink(); ?>" class="button">Read more</a>
+        <a class="read-more" href="<?php the_permalink() ?>">Read more</a>
+        <?php if (get_comments_number() != 0) { ?>
+          <span> &mdash; </span>
+          <a href="<?php comments_link() ?>"><?php printf(_n('1 comment', '%1$s comments', get_comments_number(), 'roots'), number_format_i18n(get_comments_number())) ?></a>
+        <?php } ?>
     </div>
 </article>
