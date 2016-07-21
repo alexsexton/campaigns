5ca61de62f226b155b8b8debe5c2f67f9643e6af
Alex Sexton <alex@dxw.com>
2016-07-21 14:58:57 +0100

    Refactor content blocks

--- wp-content/themes/campaign-root/templates/partials/content-blocks.php
+++ wp-content/themes/campaign-root/templates/partials/content-blocks.php
@@ -28,6 +28,8 @@
 
         <?php get_template_part('partials/content-blocks/hidden-text-block'); ?>
 
+        <?php get_template_part('partials/content-blocks/link-block'); ?>
+
   <?php // end content block loop
     endwhile;
     endif;
