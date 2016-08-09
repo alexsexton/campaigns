0a86d63d1f54baaeec593e842f328e605a135f46
Alex Sexton <alex@dxw.com>
2016-08-09 10:48:15 +0100

    Removed page categories

--- wp-content/themes/campaign-root/app/Theme/Pages.php
+++ wp-content/themes/campaign-root/app/Theme/Pages.php
@@ -4,27 +4,5 @@ namespace Dxw\GdsCampaignRoot\Theme;
 
 class Pages implements \Dxw\Iguana\Registerable
 {
-
-    public function add_taxonomies_to_pages()
-    {
-        register_taxonomy_for_object_type('category', 'page');
-    }
-
-    public function category_archives($wp_query)
-    {
-        $my_post_array = array('post','page');
-
-        if ($wp_query->get('category_name') || $wp_query->get('cat')) {
-            $wp_query->set('post_type', $my_post_array);
-        }
-    }
-
-    public function register()
-    {
-        add_action('init', [$this, 'add_taxonomies_to_pages']);
-
-        if (! is_admin()) {
-            add_action('pre_get_posts', [$this, 'category_archives']);
-        }
-    }
+    //...
 }
