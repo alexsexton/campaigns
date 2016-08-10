e695949f52378e89963ae3cabc1ba63d0371ba59
Alex Sexton <alex@dxw.com>
2016-08-10 14:11:21 +0100

    Added open graph tags partial

--- wp-content/themes/campaign-root/templates/layouts/main.php
+++ wp-content/themes/campaign-root/templates/layouts/main.php
@@ -6,11 +6,11 @@
 <head>
     <meta charset="utf-8">
     <?php wp_head(); ?>
+    <?php get_template_part('partials/open-graph-tags'); ?>
 </head>
 <body <?php body_class(); ?>>
 
     <?php get_template_part('partials/govuk-header'); ?>
-
     <?php get_template_part('partials/header'); ?>
 
     <main class="main" role="main" id="content">
@@ -18,14 +18,12 @@
     </main>
 
     <?php get_template_part('partials/footer'); ?>
-
     <?php get_template_part('partials/govuk-footer'); ?>
 
     <?php wp_footer(); ?>
     <?php if( get_field('header_background_image', 'option')) : ?>
     <?php $header_image = get_field('header_background_image', 'option'); ?>
     <script>
-    // Call backstretch on campaign header
     jQuery(function ($) {
         $(function () {
           $('.site-header').backstretch("<?php echo $header_image['url']; ?>")
