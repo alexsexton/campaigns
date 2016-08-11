c41f45c09b44273d68effac974695f7cb754c984
Alex Sexton <alex@dxw.com>
2016-08-11 16:27:58 +0100

    Modified video content and banner block

--- wp-content/themes/campaign-root/templates/partials/content-blocks/video-block.php
+++ wp-content/themes/campaign-root/templates/partials/content-blocks/video-block.php
@@ -2,7 +2,7 @@
 //
 // Content Block ### Video Block
 //
-if( get_row_layout() == 'video_content') : ?>
+if( get_row_layout() == 'video') : ?>
 
 <?php if( get_sub_field('background_colour')) : ?>
 <div id="video-block-p-<?php echo get_row_index(); ?>" class="video-block content-block" style="background-color:<?php the_sub_field('background_colour'); ?>;">
@@ -10,7 +10,13 @@ if( get_row_layout() == 'video_content') : ?>
 <div id="video-block-p-<?php echo get_row_index(); ?>" class="video-block content-block">
 <?php endif; ?>
     <div class="row">
-        <div class="fitvids"><?php the_sub_field('video'); ?></div>
+        <div class="fitvids">
+          <?php
+            $fullpath = get_sub_field('video_link');
+            $videoid = preg_split('/(\?[v]\=)/', $fullpath);
+          ?>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $videoid[1]; ?>?feature=oembed" frameborder="0" allowfullscreen></iframe>
+        </div>
     </div>
 </div>
 <?php endif; ?>
