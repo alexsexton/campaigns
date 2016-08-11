5c5e62c285e7efc0951ff38e426c951126e75bff
Alex Sexton <alex@dxw.com>
2016-08-11 16:52:45 +0100

    Added call to action button block

---
+++ wp-content/themes/campaign-root/templates/partials/content-blocks/call-to-action-button.php
@@ -0,0 +1,15 @@
+<?php
+//
+// Content Block ### Call to Action Button
+//
+if( get_row_layout() == 'call_to_action_button') : ?>
+?>
+
+<div id="call-to-action-button-p-<?php echo get_row_index(); ?>" class="call-to-action-button">
+    <div class="row">
+        <div class="buttons">
+            <a href="<?php the_sub_field('button_url'); ?>" class="button" <?php if( get_sub_field('ga_event_tracking') == true): ?> onclick="ga('send', 'event', '<?php the_sub_field('event_category'); ?>', '<?php the_sub_field('event_action'); ?>', '<?php the_sub_field('event_label'); ?>');"<?php endif; ?>><?php the_sub_field('button_text'); ?></a>
+        </div>
+    </div>
+</div>
+<?php endif; ?>
