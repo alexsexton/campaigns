60cad79ead4ffa202d1ff97ac24b1027bd7b6466
Alex Sexton <alex@dxw.com>
2016-08-22 18:03:07 +0100

    Added image size and url options to image and caption blocks

--- wp-content/themes/campaign-root/templates/partials/content-blocks/right-aligned-image-block.php
+++ wp-content/themes/campaign-root/templates/partials/content-blocks/right-aligned-image-block.php
@@ -3,19 +3,22 @@
 // Content Block ### Right-Aligned Image and Caption
 //
 if( get_row_layout() == 'right_aligned_image') : ?>
-    <div id="right-image-caption-p-<?php echo get_row_index(); ?>" class="right-image-caption">
+    <div id="right-image-caption-p-<?php echo get_row_index(); ?>" class="right-image-caption <?php if( get_sub_field('image_size') != 'default') { the_sub_field('image_size'); } ?>">
         <div class="row">
 
           <?php $right_aligned_image = get_sub_field('image'); ?>
 
           <figure>
-              <img src="<?php echo $right_aligned_image['url']; ?>" alt="<?php echo $right_aligned_image['alt']; ?>">
+            <?php if( get_sub_field('image_url')): ?> <a href="<?php the_sub_field('image_url'); ?>"> <?php endif ?>
+                <img src="<?php echo $right_aligned_image['url']; ?>" alt="<?php echo $right_aligned_image['alt']; ?>">
+            <?php if( get_sub_field('image_url')): ?> </a> <?php endif ?>
             <?php if( $right_aligned_image['caption'] ) : ?>
             <figcaption>
               <?php if( $right_aligned_image['title'] ) : ?>
-                <h3><?php echo $right_aligned_image['title']; ?></h3>
+                <h3><?php if( get_sub_field('image_url')): ?> <a href="<?php the_sub_field('image_url'); ?>"> <?php endif ?><?php echo $right_aligned_image['title']; ?>
+                <?php if( get_sub_field('image_url')): ?> </a> <?php endif ?></h3>
               <?php endif; ?>
-              <?php echo $right_aligned_image['caption']; ?>
+              <p><?php echo $right_aligned_image['caption']; ?></p>
             </figcaption>
             <?php endif; ?>
           </figure>
