f6a8a580ed51326a95e868a916e238da029612fe
Alex Sexton <alex@dxw.com>
2016-09-15 11:37:23 +0100

    Modified filter to add GA before body and GTM after body

--- wp-content/themes/campaign-root/templates/layouts/main.php
+++ wp-content/themes/campaign-root/templates/layouts/main.php
@@ -7,14 +7,8 @@
     <meta charset="utf-8">
     <?php wp_head(); ?>
     <?php get_template_part('partials/open-graph-tags'); ?>
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-38052350-1', 'auto');
-        ga('send', 'pageview');
-    </script>
+    <?php do_action('campaigns_before_body_gds_analytics') ?>
+    <?php do_action('campaigns_before_body') ?>
 </head>
 <body <?php body_class(); ?>>
     <?php do_action('campaigns_after_body') ?>
