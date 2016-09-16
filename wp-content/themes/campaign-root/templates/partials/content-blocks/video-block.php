2c0ae2c1d380c9863efe2e65f8b25c4eee14bf45
Alex Sexton <alex@dxw.com>
2016-09-16 11:59:27 +0100

    Added title and caption field to video block

--- wp-content/themes/campaign-root/templates/partials/content-blocks/video-block.php
+++ wp-content/themes/campaign-root/templates/partials/content-blocks/video-block.php
@@ -10,6 +10,9 @@ if( get_row_layout() == 'video') : ?>
 <div id="video-block-p-<?php echo get_row_index(); ?>" class="video-block">
 <?php endif; ?>
     <div class="row">
+        <?php if( get_sub_field('video_title')) : ?>
+            <h2><?php the_sub_field('video_title'); ?></h2>
+        <?php endif; ?>
         <div class="fitvids">
           <?php
             $fullpath = get_sub_field('video_link');
@@ -17,6 +20,11 @@ if( get_row_layout() == 'video') : ?>
           ?>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $videoid[1]; ?>?feature=oembed" frameborder="0" allowfullscreen></iframe>
         </div>
+        <?php if( get_sub_field('video_caption')) : ?>
+        <div class="rich-text">
+            <?php the_sub_field('video_caption'); ?>
+        </div>
+        <?php endif; ?>
     </div>
 </div>
 <?php endif; ?>
