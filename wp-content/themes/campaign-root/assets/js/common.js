97e605ab3ebb2ffa42ff6b31a5ca891375c03a42
Alex Sexton <alex@dxw.com>
2016-09-16 12:00:49 +0100

    Added cookie notification script

--- wp-content/themes/campaign-root/assets/js/common.js
+++ wp-content/themes/campaign-root/assets/js/common.js
@@ -1,1 +1,20 @@
+'use strict'
 
+jQuery(function ($) {
+  // Cookie message
+  var cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)cookieMessage\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+
+  var cookieBanner = $("#global-cookie-message");
+
+  cookieBanner.click(function(e){
+      e.preventDefault;
+      $(this).addClass("visually-hidden");
+  })
+
+  if (cookieValue == "true") {
+      cookieBanner.addClass("visually-hidden");
+  } else {
+      cookieBanner.removeClass("visually-hidden");
+      document.cookie = "cookieMessage=true";
+  }
+})
