f5542a7fb780105e12580488142b7555c70e3ece
Alex Sexton <alex@dxw.com>
2016-09-19 14:55:41 +0100

    Added conditional

--- wp-content/themes/campaign-root/templates/partials/content-blocks/full-width-image-block.php
+++ wp-content/themes/campaign-root/templates/partials/content-blocks/full-width-image-block.php
@@ -9,6 +9,7 @@ if( get_row_layout() == 'full_width_image') : ?>
       <?php $full_width_image = get_sub_field('image'); ?>
         <figure>
           <img src="<?php echo $full_width_image['url']; ?>" alt="<?php echo $full_width_image['alt']; ?>">
+          <?php if ($full_width_image['title'] || $full_width_image['caption']) : ?>
           <figcaption>
               <?php if ($full_width_image['title']) {
               echo '<h2>' . $full_width_image['title'] . '</h2>';
@@ -17,6 +18,7 @@ if( get_row_layout() == 'full_width_image') : ?>
               echo '<p>' . $full_width_image['caption'] . '</p>';
               } ?>
           </figcaption>
+          <?php endif; ?>
         </figure>
     </div>
 </div>
