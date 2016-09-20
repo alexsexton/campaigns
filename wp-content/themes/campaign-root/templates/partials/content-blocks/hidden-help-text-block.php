69f20f055396d1bbb02678969a589ca465c2fcd8
Alex Sexton <alex@dxw.com>
2016-09-20 11:54:15 +0100

    Fixed hidden help text for older browsers

--- wp-content/themes/campaign-root/templates/partials/content-blocks/hidden-help-text-block.php
+++ wp-content/themes/campaign-root/templates/partials/content-blocks/hidden-help-text-block.php
@@ -7,17 +7,17 @@ if( get_row_layout() == 'hidden_help_text') : ?>
 <?php $hidden_text_colour = get_sub_field('hidden_help_text'); ?>
 
 <?php if( get_sub_field('background_colour')) : ?>
-<div id="hidden-help-text-p-<?php echo get_row_index(); ?>" class="hidden-help-text <?php echo $hidden_text_colour;?>" style="background-color:<?php the_sub_field('background_colour'); ?>;">
-<?php else : ?>
-<div id="hidden-help-text-p-<?php echo get_row_index(); ?>" class="hidden-help-text <?php echo $hidden_text_colour;?>">
-<?php endif; ?>
-    <div class="row">
-        <details>
-          <summary><span class="summary"><?php the_sub_field('summary'); ?></span></summary>
-          <div class="panel">
-              <p><?php the_sub_field('details'); ?></p>
-          </div>
-        </details>
+    <div id="hidden-help-text-p-<?php echo get_row_index(); ?>" class="hidden-help-text <?php echo $hidden_text_colour;?>" style="background-color:<?php the_sub_field('background_colour'); ?>;">
+    <?php else : ?>
+        <div id="hidden-help-text-p-<?php echo get_row_index(); ?>" class="hidden-help-text <?php echo $hidden_text_colour;?>">
+        <?php endif; ?>
+        <div class="row">
+            <div class="details">
+                <div class="summary"><?php the_sub_field('summary'); ?></div>
+                <div class="panel">
+                    <?php the_sub_field('details'); ?>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
 <?php endif; ?>
