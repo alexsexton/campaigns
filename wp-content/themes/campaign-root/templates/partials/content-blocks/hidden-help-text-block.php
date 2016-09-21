4e52d966a8a3c8183890e9448bfd2fc758388e6d
Alex Sexton <alex@dxw.com>
2016-09-21 10:13:28 +0100

    Added namespace to acf field

--- wp-content/themes/campaign-root/templates/partials/content-blocks/hidden-help-text-block.php
+++ wp-content/themes/campaign-root/templates/partials/content-blocks/hidden-help-text-block.php
@@ -6,8 +6,8 @@ if( get_row_layout() == 'hidden_help_text') : ?>
 
 <?php $hidden_text_colour = get_sub_field('hidden_help_text'); ?>
 
-<?php if( get_sub_field('background_colour')) : ?>
-    <div id="hidden-help-text-p-<?php echo get_row_index(); ?>" class="hidden-help-text <?php echo $hidden_text_colour;?>" style="background-color:<?php the_sub_field('background_colour'); ?>;">
+<?php if( get_sub_field('hidden_help_text_background_colour')) : ?>
+    <div id="hidden-help-text-p-<?php echo get_row_index(); ?>" class="hidden-help-text <?php echo $hidden_text_colour;?>" style="background-color:<?php the_sub_field('hidden_help_text_background_colour'); ?>;">
     <?php else : ?>
         <div id="hidden-help-text-p-<?php echo get_row_index(); ?>" class="hidden-help-text <?php echo $hidden_text_colour;?>">
         <?php endif; ?>
